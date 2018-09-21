647e8a23b7ca3c6dcd7d3343969c5682568ab234
Javier Calzado <fjcalzadorodriguez@gmail.com>
2018-09-21 09:48:33 +0200

    Boilerplate

---
+++ src/validator.ts
@@ -0,0 +1,1 @@
+export const validator = (input: string) => true;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+
+const capitalizeString = (s) => s.charAt(0).toUpperCase() + s.slice(1);
+
+module.exports = (env) => {
+  const NODE_ENV = process.env.NODE_ENV;
+  const mode = NODE_ENV || 'development';
+  const packageName = process.env.npm_package_name;
+  const packageNameCapital = packageName.split('-').map(capitalizeString).join('');
+  const packageVersion = JSON.stringify(process.env.npm_package_version).replace(/"/g, '');
+  const filename = `${packageName}-${packageVersion}${mode === 'production' ? '.min' : ''}.js`;
+
+  return ({
+    mode,
+    resolve: {
+      extensions: ['.js', '.ts']
+    },
+    entry: ['./src/index.ts'],
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename,
+      library: {
+        root: packageNameCapital,
+        amd: packageName,
+        commonjs: packageName,
+      },
+      libraryTarget: 'umd',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(tsx?)|(js)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+  });
+};
